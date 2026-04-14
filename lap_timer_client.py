1b4d06ff79aa38ed663d40b211456319f21355a2
tomas londoño <tomaslonco@unisabana.edu.co>
2026-04-14 11:03:46 -0500

    parcial.2

--- lap_timer_client.py
+++ lap_timer_client.py
@@ -6,16 +6,17 @@ import lap_timer
 
 
 def main():
-    # TODO: Pedir el nombre del archivo al usuario usando input()
+    name = input("Ingrese el nombre del archivo: ")
     
-    # TODO: Abrir el archivo y leer el numero de vueltas n
+    with open(name, 'r') as f:
+        n = int(f.readline().strip())
+        timer = lap_timer.init(n)
+
+        for _ in range(n):
+            time = float(f.readline().strip())
+            timer = lap_timer.add_lap(timer, time)
     
-    # TODO: Crear el cronometro usando lap_timer.init(n)
-    
-    # TODO: Leer los n tiempos de vuelta y agregarlos con lap_timer.add_lap()
-    
-    # TODO: Imprimir la racha decreciente mas larga
-    #       usando lap_timer.longest_decreasing_streak()
+    print("Tiempos de vuelta:", lap_timer.format_laps(timer))
     
     pass
 
